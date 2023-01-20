55ae7e4c7a11c8345f8b3e79fe33c5773c101e34
aquaprogit <vladd.golovatyuk@gmail.com>
2023-01-20 15:58:03 +0200

    validating for dog

---
+++ DogAPI/DogAPI/Validators/DogValidator.cs
@@ -0,0 +1,24 @@
+﻿using DogAPI.DAL.Entities;
+
+using FluentValidation;
+
+namespace DogAPI.Validators;
+
+public class DogValidator : AbstractValidator<Dog>
+{
+    public DogValidator()
+    {
+        RuleFor(d => d.Name).NotEmpty()
+                            .MinimumLength(1)
+                            .MaximumLength(50)
+                            .Matches(@"([A-Z]|[А-Я]){1}([a-z]|[а-я])*");
+
+        RuleFor(d => d.TailLength).GreaterThan(0);
+
+        RuleFor(d => d.Weight).GreaterThan(0);
+
+        RuleFor(d => d.Color).NotEmpty()
+            .MinimumLength(1)
+            .MaximumLength(50);
+    }
+}
